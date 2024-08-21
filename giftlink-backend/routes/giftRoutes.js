36a3d1bf1503990a3277170c973a05698869b08e
Skylar McCowan <skylarmccowan4@gmail.com>
2024-08-21 14:45:27 -0500

    Update giftRoutes.js

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,3 +1,53 @@
-git add routes/giftRoutes.js
-git commit -m "Configured JSHint for ES8 and fixed linting issues in giftRoutes.js"
-git push origin main
+/* jshint esversion: 8 */
+
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+const logger = require('../logger');
+
+// Get all gifts
+router.get('/', async (req, res, next) => {
+    logger.info('/ called');
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const gifts = await collection.find({}).toArray();
+        res.json(gifts);
+    } catch (e) {
+        logger.error('oops something went wrong', e);  // Added missing semicolon
+        next(e);
+    }
+});
+
+// Get a single gift by ID
+router.get('/:id', async (req, res, next) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const id = req.params.id;
+        const gift = await collection.findOne({ id: id });
+
+        if (!gift) {
+            return res.status(404).send("Gift not found");
+        }
+
+        res.json(gift);
+    } catch (e) {
+        next(e);
+    }
+});
+
+// Add a new gift
+router.post('/', async (req, res, next) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const gift = await collection.insertOne(req.body);
+
+        res.status(201).json(gift.ops[0]);
+    } catch (e) {
+        next(e);
+    }
+});
+
+module.exports = router;
